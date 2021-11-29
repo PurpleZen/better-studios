59c143cf811d540716c494a6a43c9dc1ced11f25
Leo <87787046+LegoManiac04@users.noreply.github.com>
2021-11-29 15:09:15 -0800

    push

---
+++ bg-fix.js
@@ -0,0 +1,1 @@
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-page { background-color: #fcfcfc} .replies.collapsed > .comment:last-of-type:after { background: none}"})));
